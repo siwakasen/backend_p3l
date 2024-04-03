ef87e2c60028b8f069c17a256b580119f434bf64
ApocalCrk <ferdyfirmansyah3026@gmail.com>
2024-04-03 20:18:53 +0700

    Add 'status' and 'tanggal' columns to 'presensi' table

--- database/migrations/2024_04_03_035426_create_presensi_table.php
+++ database/migrations/2024_04_03_035426_create_presensi_table.php
@@ -14,6 +14,8 @@ return new class extends Migration
         Schema::create('presensi', function (Blueprint $table) {
             $table->id('id_presensi');
             $table->foreignId('id_karyawan')->references('id_karyawan')->on('karyawan')->cascadeOnDelete()->cascadeOnUpdate();
+            $table->string('status');
+            $table->date('tanggal');
             $table->timestamps();
         });
     }
