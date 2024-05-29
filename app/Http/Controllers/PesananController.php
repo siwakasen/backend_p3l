08081e05e5ad568c17e6abe370d618a3d2178e51
ApocalCrk <ferdyfirmansyah3026@gmail.com>
2024-05-30 00:32:17 +0700

    bug: fixing minor bug

--- app/Http/Controllers/PesananController.php
+++ app/Http/Controllers/PesananController.php
@@ -550,7 +550,7 @@ class PesananController extends Controller
                     $subQuery->where('status_transaksi', 'Menunggu Konfirmasi Pesanan')
                              ->where('metode_pengiriman', 'Pengantaran Kurir Toko');
                     })->orWhere('status_transaksi', 'Menunggu Konfirmasi Pembayaran');
-                })->orderBy('created_at', 'desc')
+                })->orderBy('tanggal_pesanan', 'desc')
                 ->get()
                 ->load('User')
                 ->load('DetailPesanan')
