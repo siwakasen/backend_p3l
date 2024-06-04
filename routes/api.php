4a1e87b817e46e160e26987497ce332fd56cd969
siwakasen <siwakasen@gmail.com>
2024-06-04 18:55:25 +0700

    complete laporan cashflow & penitip

--- routes/api.php
+++ routes/api.php
@@ -304,7 +304,8 @@ Route::prefix('administrator')->group(function () {
     */
     Route::prefix('laporan')->group(function(){
         Route::get('/presensi-karyawan/{tahun}/{bulan}', [LaporanController::class, 'laporanPresensiKaryawan'])->middleware('auth:sanctum', 'ability:owner,manajer-operasional');
-
+        Route::get('/transaksi-penitip/{tahun}/{bulan}', [LaporanController::class, 'laporanTransaksiPenitip'])->middleware('auth:sanctum', 'ability:owner,manajer-operasional');
+        Route::get('/cashflow/{tahun}/{bulan}', [LaporanController::class, 'laporanCashflow'])->middleware('auth:sanctum', 'ability:owner,manajer-operasional');
     });
 });
 
